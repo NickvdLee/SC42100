8c6cbcc507e2a9f2b41c16ab14356c84ee13f3c1
Nick <n.a.vanderlee@student.tudelft.nl>
2020-05-11 20:55:08 +0200

    Fix typo, update proper PageRank horizon.

--- task3.m
+++ task3.m
@@ -13,11 +13,11 @@ P = diag(1./sum(W,2))*W; % Fast inverse
 mu = ones(l,1);
 beta = 0.15; % Typical value
 
-PageRank = mu; % Typical i  nitialization?
+PageRank = mu; % Typical initialization?
 M = (1-beta)*P'; % Cache these
-num_plots = beta*mu;
-for i=1:10
-    PageRank = M*PageRank + num_plots;
+N = beta*mu;
+for i=1:100
+    PageRank = M*PageRank + N;
 end
 % Pagerank = (eye(l)-M)\N; SS solution
 
