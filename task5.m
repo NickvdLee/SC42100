1aef746cfbf9d41f3e06a562d07a5e56227d3240
Nick <n.a.vanderlee@student.tudelft.nl>
2020-05-28 17:05:23 +0200

    Add some plots

--- task5.m
+++ task5.m
@@ -1,7 +1,7 @@
 close all; clear all; clc;
 addpath('functions/');
-addpath('cvx/');
-cvx_startup
+% addpath('cvx/');
+% cvx_startup
 load -ascii traffic.mat 
 load -ascii traveltime.mat
 load -ascii capacities.mat
@@ -10,17 +10,17 @@ load -ascii flow.mat
 
 %% Setup graph G
 % get edges from B
-Links = zeros(size(traffic,2),3);
+Links = zeros(size(traffic,2),2);
 for i = 1:size(traffic,2)
     Links(i,1) = find(traffic(:,i) == 1);
     Links(i,2) = find(traffic(:,i) == -1);
 end
 
 % also minimum travel time
-Links(:,3) = traveltime;
+% Links(:,3) = traveltime;
 
 % Make graph using empty network weights
-G = digraph(Links(:,1)', Links(:,2)', Links(:,3)');
+G = digraph(Links(:,1)', Links(:,2)', traveltime');
 
 %% 5.a
 [p1_13, d1_13, e1_13]  = shortestpath(G,1,13);
@@ -29,8 +29,7 @@ G = digraph(Links(:,1)', Links(:,2)', Links(:,3)');
 % matlab will treat the weight as the capacity of the edge. Therefore the
 % weight vector needs to be swapped out for the capacity vector. 
 
-Links(:,3) = capacities;
-G = digraph(Links(:,1)',Links(:,2), Links(:,3)');
+G = digraph(Links(:,1)',Links(:,2)', capacities');
 [mf1_13,~] = maxflow(G,1,13);
 
 %% 5.c
@@ -45,7 +44,7 @@ lambda = [nu(1) zeros(1,size(traffic,1)-1)]';
 mu = [zeros(1,size(traffic,1)-1) nu(1)]';
 One = ones(M,1);
 
-cvx_begin
+cvx_begin quiet
     variable f(M)
     minimize sum((l.*c).*inv_pos(One-f.*(1.*inv_pos(c))))
     subject to
@@ -54,22 +53,30 @@ cvx_begin
 cvx_end
 fstar = f;
 
+Gplot = digraph(Links(:,1)', Links(:,2)', fstar');
+W = Gplot.Edges.Weight./max(Gplot.Edges.Weight);
+h = plot(Gplot,'k','EdgeLabel',round(Gplot.Edges.Weight),'LineWidth',3*W,'ArrowSize',15*W,'Layout','force');
 
 %% 5.e
-cvx_begin
+cvx_begin quiet
     variable f(M)
-    minimize sum(-l.*c.*log(c-f)/log(exp(1)))
+    minimize sum(-l.*c.*log(c-f))
     subject to
         B*f == lambda - mu
         0 <= f <= c
 cvx_end
-fo = f;
+fw = f;
+
+Gplot = digraph(Links(:,1)', Links(:,2)', fw');
+W = Gplot.Edges.Weight./max(Gplot.Edges.Weight);
+h = plot(Gplot,'k','EdgeLabel',round(Gplot.Edges.Weight),'LineWidth',3*W,'ArrowSize',15*W,'Layout','force');
 
 %% 5.f
 dacc = c.*l./((c-fstar).*(c-fstar));
+% dacc = l.*fstar./c./((fstar./c-1).^2);
 w = fstar.*dacc;
 
-cvx_begin
+cvx_begin quiet
     variable f(M)
     minimize sum(-l.*c.*log(c-f)+w.*f)
     subject to
