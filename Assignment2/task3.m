0a8dc6ba38cfd7c0eb9e1bfdc73b57c50d36a80e
Nick <n.a.vanderlee@student.tudelft.nl>
2020-06-10 22:01:04 +0200

    Doesnt work!

--- Assignment2/task3.m
+++ Assignment2/task3.m
@@ -18,46 +18,66 @@ x0 = {x01, x02, x03, x04};
 % Systems are not stable
 
 %% MPC
-alpha = 0.5;      % step size
-iterations = 20;
-
+alpha = 0.00025;      % step size
+iterations = 100;
 
 % save xf over all iterations and initialise. 
-xfs = cell(1,iterations);
-xfs(1,:) = {zeros(4,1)};
+xfs = cell(4,iterations);
+xfs(:,:) = {zeros(4,1)};
 
+%%
 % Set up lambda
-lambda = cell(4,iterations);
-lambda(:,:) = {ones(1,4)};
+lambda = cell(4,4,iterations);
+lambda(:,:,:) = {ones(4,1)};
+for k=1:n
+    lambda(k,k,:) = {zeros(4,1)};
+end
 
 % save inputs and cost for all nodes
 inputs = cell(4,1);
 cost = cell(4,1);
-
+%%
 % Solve the problem in one time step:
-for i = 1:iterations
-    for j = 1:4
+for k = 1:iterations
+    for i = 1:4
         % Making S and T. X_N+1 = Tx_0 + Su_n
-        [T,S,W] = mpc_mtrx(A{j},B{j},Tfinal);
+        [T,S,W] = mpc_mtrx(A{i},B{i},Tfinal);
         % x = Tx0 + Su
         % xN = A^N x0 + Wu
-
+        
         % Optimising over the control input. 
         cvx_begin quiet
             variable u(2*Tfinal,1)
-            variable Lambda(1,4)
-            minimize((T*x0{j}+S*u)'*(T*x0{j}+S*u) + u'*u)% + Lambda*((A{j}^Tfinal*x0{j}+W*u) - xfs{1,i}))
+            x = T*x0{i} + S*u;
+            xf = A{i}^Tfinal*x0{i}+W*u;
+            lij = 0;            
+            lji = 0;
+            for j=1:n
+                lij = lij + lambda{i,j,k};
+                lji = lji + lambda{j,i,k};
+            end
+            minimize(x'*x + u'*u + lij'*xf - lji'*xf)
             subject to
-                A{j}^Tfinal*x0{j}+W*u == xfs{1,i};
                 u'*u <= umax^2;
         cvx_end
         
-        % save for all nodes
-        cost{j} = (T*x0{j}+S*u)'*(T*x0{j}+S*u) + u'*u;
-        inputs{j} = u;
-        lambda{j,i} = Lambda;
-        disp(['i: ',num2str(i),' j: ',num2str(j)])
+        inputs{i} = u;
+        xfs{i,k} = xf;
+        disp(['i: ',num2str(k),' j: ',num2str(i)])
     end
-    xfs{1,i+1} = xfs{1,i} - alpha*(lambda{1,i}' - lambda{2,i}');      % update xf based on subgradient. 
+    
+    % Update lij and lji
+    for i=1:n
+        for j=1:n
+            if i==j
+                continue
+            end
+            lambda{i,j,k+1} = lambda{i,j,k} + alpha*(xfs{i,k}-xfs{j,k});
+            lambda{j,i,k+1} = lambda{j,i,k} + alpha*(xfs{j,k}-xfs{i,k});
+        end
+    end
+
+    disp('Final states:');
+    disp([xfs{1,k} xfs{2,k} xfs{3,k} xfs{4,k}]);
 end
 
