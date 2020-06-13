e5f95e30295a7bd1234f6a2bd7a0ab255343a891
Nick <n.a.vanderlee@student.tudelft.nl>
2020-06-13 12:01:41 +0200

    commit just to be sure

--- Assignment2/task3.m
+++ Assignment2/task3.m
@@ -18,36 +18,35 @@ x0 = {x01, x02, x03, x04};
 % Systems are not stable
 
 %% MPC
-alpha = 0.5;      % step size
-iterations = 500;
+alpha = .15;      % step size
+iterations = 100;
 
 % save xf over all iterations and initialise. 
 xfs = cell(4,iterations);
 xfs(:,:) = {zeros(4,1)};
+xs = cell(4,iterations);
+xs(:,:) = {zeros((Tfinal+1)*size(A1,1),1)};
 
 %%
 % Set up lambda
 lambda = cell(n,n,iterations);
 lambda(:,:,:) = {ones(4,1)};
-for k=1:n
-    lambda(k,k,:) = {zeros(4,1)};
+for r=1:n
+    lambda(r,r,:) = {zeros(4,1)};
 end
 
 % save inputs and cost for all nodes
 inputs = cell(n,1);
 cost = cell(n,1);
-%%
+
 % Solve the problem in one time step:
-T = cell(1,n);
-S = cell(1,n);
-W = cell(1,n);
+T = cell(1,n); S = cell(1,n); W = cell(1,n);
 for i = 1:n
     [t,s,w] = mpc_mtrx(A{i},B{i},Tfinal);
-    T{i} = t;
-    S{i} = s;
-    W{i} = w;
+    T{i} = sparse(t); S{i} = sparse(s); W{i} = w;
 end
-for k = 1:iterations
+
+for r = 1:iterations
     for i = 1:n
         % Making S and T. X_N+1 = Tx_0 + Su_n
 %         [T,S,W] = mpc_mtrx(A{i},B{i},Tfinal);
@@ -62,18 +61,19 @@ for k = 1:iterations
             lij = 0;            
             lji = 0;
             for j=1:n
-                lij = lij + lambda{i,j,k};
-                lji = lji + lambda{j,i,k};
+                lij = lij + lambda{i,j,r};
+                lji = lji + lambda{j,i,r};
             end
             theta = repmat(xf,[Tfinal,1]);
-            minimize((x-theta)'*(x-theta) + u'*u + lij'*xf - lji'*xf)
+            minimize(x'*x + xf'*xf + u'*u + lij'*xf - lji'*xf)
             subject to
                 u'*u <= umax^2;
         cvx_end
         
         inputs{i} = u;
-        xfs{i,k} = xf;
-        disp(['i: ',num2str(k),' j: ',num2str(i)])
+        xfs{i,r} = xf;
+        xs{i,r} = [T{i}*x0{i}+S{i}*u;xf];
+        disp(['i: ',num2str(r),' j: ',num2str(i)])
     end
     
     % Update lij and lji
@@ -82,13 +82,25 @@ for k = 1:iterations
             if i==j
                 continue
             end
-            lambda{i,j,k+1} = lambda{i,j,k} + alpha*(xfs{i,k}-xfs{j,k});
-            lambda{j,i,k+1} = lambda{j,i,k} + alpha*(xfs{j,k}-xfs{i,k});
+            lambda{i,j,r+1} = lambda{i,j,r} + alpha*(xfs{i,r}-xfs{j,r});
+            lambda{j,i,r+1} = lambda{j,i,r} + alpha*(xfs{j,r}-xfs{i,r});
         end
     end
 
     disp('Final states:');
-    disp([xfs{1,k} xfs{2,k} xfs{3,k} xfs{4,k}]);
-    disp(norm(diff([xfs{1,k} xfs{2,k} xfs{3,k} xfs{4,k}],n-1,2)));
+    disp([xfs{1,r} xfs{2,r} xfs{3,r} xfs{4,r}]);
+    disp('Convergence:');
+    disp(norm(diff([xfs{1,r} xfs{2,r} xfs{3,r} xfs{4,r}],n-1,2)));
 end
 
+%% Plots
+figure(1)
+colors = {[0 0 1],[1 0 0],[0 1 0],[0 1 1]};
+for r = 1:100
+    for i=1:4
+        X = reshape(xs{i,r},[4 6]);
+        plot(X(1,:),X(2,:),'.','Color',colors{i}*r/100);
+        hold on
+    end
+%     hold off
+end
